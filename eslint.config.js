36dc717d3d1f15f146139633f2f8f0271dbe192c
Alex <alex@example.com>
2025-03-09 22:07:21 +0300

    logic

--- eslint.config.js
+++ eslint.config.js
@@ -3,17 +3,30 @@ import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default [
+  // Определение файлов для линтинга
   {
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
   },
 
+  // Игнорируемые файлы и директории
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
 
+  // Основная конфигурация ESLint
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   skipFormatting,
+
+  // Настройка правил ESLint
+  {
+    rules: {
+      'vue/no-multiple-template-root': 'off', //
+      'vue/multi-word-component-names': 'off', // Отключаем правило
+      // Отключаем правило
+      // Добавьте другие правила здесь
+    },
+  },
 ]
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,12 +1,34 @@
 <script setup>
 import DrawerHead from './DrawerHead.vue'
-import Cartitem from './Cartitem.vue'
+import CartItemList from './CartItemList.vue'
 </script>
 
 <template>
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
     <DrawerHead />
-    <Cartitem />
+
+    <CartItemList />
+
+    <div class="flex flex-col gap-4 mt-7">
+      <div class="flex gap-2">
+        <span>Итого:</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>12990 ₽</b>
+      </div>
+
+      <div class="flex gap-2">
+        <span>Налог 5%:</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>900 ₽</b>
+      </div>
+
+      <button
+        disabled="true"
+        class="mt-4 bg-lime-500 w-full rounded-xl py-3 cursor-pointer text-white hover:bg-lime-600 transition active:bg-lime-700 disabled:bg-slate-300 disabled:cursor-not-allowed"
+      >
+        Оформить заказ
+      </button>
+    </div>
   </div>
 </template>
