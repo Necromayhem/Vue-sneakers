8a596762f06d0ca42a8ba2e0d6b859a34f3dec33
Alex <alex@example.com>
2025-03-09 12:20:54 +0300

    first commit

---
+++ src/components/CardList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert(111)
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      image-url="/sneakers/sneakers-1.jpg"
+      :price="5000"
+      :is-favorite="false"
+      :is-added="false"
+      :onClickAdd="onClickAdd"
+    />
+  </div>
+</template>
---
+++ src/components/Cartitem.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="border border-slate-200 p-4 rounded-xl">
+    <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
+
+    <div flex></div>
+  </div>
+</template>
